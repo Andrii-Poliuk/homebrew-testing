04e3bdaba8c65c86a6e342499bb8fdc2e5cb4d28
Admin <andrii.polyuk@blackthorn-vision.com>
2016-05-27 13:55:11 +0300

    Added formula

---
+++ brewtesting.rb
@@ -0,0 +1,12 @@
+class Brewtesting < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/Andrii-Poliuk/brewtesting/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "99e6ba45a4a7df49f0956dfe9bfbeaa46779e43b34a5f065b0f6078ac075f64a"
+
+  def install
+   bin.install 'Nyan.png'
+  end
+
+end
